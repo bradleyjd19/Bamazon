e755e2f5f488f1069b844fa97ae977fd35f07325
Joshua Bradley <bradleyjd19@gmail.com>
2019-05-12 20:49:31 -0500

    added line breaks and command line decoration

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -52,49 +52,63 @@ function beginOrder(res) {
       beginOrder(res);
     } else {
       log(blue("You selected " + result.product_name));
+      log(blue("=".repeat(80)));
       inquirerQuant(user);
       function inquirerQuant(user) {
-      inquirer.prompt(
-        {
-          name: "orderQuant",
-          message: "Please enter a quantity"
-        }
-      ).then(function (user) {
-        const quant = user.orderQuant;
-        if (isNaN(quant) || quant < 1) {
-          log(red("Please enter a valid quantity"));
-          inquirerQuant();
-        } else {
-          log(blue("You selected " + quant));
-          if (quant > result.stock_quantity) {            
-            log("Sorry, insufficient quantity to fulfill this request, " + red(result.stock_quantity) + " currently in stock");
-          } else {
-            log(green("Items are in stock and ready for shipment!"));
-            let updateInv = "UPDATE products SET stock_quantity = ? WHERE item_id = ?"
-            let data = [(result.stock_quantity - quant), item];
-            connection.query(updateInv, data, function (err) {
-              if (err) throw err;
-              log("Inventory has been updated!");
-              log("Your total for this transaction will be " + yellow("$") + yellow((quant * result.price).toFixed(2)));
-              inquirer.prompt(
-                {
-                  type: "confirm",
-                  name: "restart",
-                  message: "Would you like to purchase any more products?"
-                }
-              ).then(function (user) {
-                if (!user.restart) {
-                  log("Thank you for shopping with Bamazon! See you next time!")
-                  connection.end();
-                } else {
-                  bamazon();
-                }
-              })
-            });
+        inquirer.prompt(
+          {
+            name: "orderQuant",
+            message: "Please enter a quantity"
           }
-        }
-      })
+        ).then(function (user) {
+          const quant = user.orderQuant;
+          if (isNaN(quant) || quant < 1) {
+            log(red("Please enter a valid quantity"));
+            inquirerQuant(res);
+          } else {
+            log(blue("You selected " + quant));
+            log(blue("=".repeat(80)));
+            if (quant > result.stock_quantity) {
+              log("Sorry, insufficient quantity to fulfill this request, " + red(result.stock_quantity) + " currently in stock");
+              log("=".repeat(80));
+              if (result.stock_quantity === 0) {
+                log(red("We are currently out of this product, please select another"));
+                log(red("=".repeat(80)));
+                bamazon();
+              } else {
+                inquirerQuant(res);
+              }
+            } else {
+              log(green("Items are in stock and ready for shipment!"));
+              log(green("=".repeat(80)));
+              let updateInv = "UPDATE products SET stock_quantity = ? WHERE item_id = ?"
+              let data = [(result.stock_quantity - quant), item];
+              connection.query(updateInv, data, function (err) {
+                if (err) throw err;
+                log(green("Inventory has been updated!"));
+                log(green("=".repeat(80)));
+                log(green("Your total for this transaction will be ") + red("$") + red((quant * result.price).toFixed(2)));
+                log(yellow("=".repeat(80)));
+                inquirer.prompt(
+                  {
+                    type: "confirm",
+                    name: "restart",
+                    message: "Would you like to purchase any more products?"
+                  }
+                ).then(function (user) {
+                  if (!user.restart) {
+                    log("=".repeat(80));
+                    log(blue("Thank you for shopping with Bamazon! See you next time!"));
+                    connection.end();
+                  } else {
+                    bamazon();
+                  }
+                })
+              });
+            }
+          }
+        })
+      }
     }
-  }
   })
 }
